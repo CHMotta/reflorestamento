62119fd03aae840dba985959152f635fa1af7f40
Carlos Motta <chmottaa@gmail.com>
2025-05-05 15:14:58 -0300

    Projeto finalizado 1.0
    Finalização da pagina relatório e busca de usuario.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let btnLogin = document.querySelector('.btn-login')
 
 //trecho que fara troca de tema
 opcoesArvores.forEach(temaPadrao => {
-    temaPadrao.addEventListener('change',()=>{
-        if(temaPadrao.checked){
+    temaPadrao.addEventListener('change', () => {
+        if (temaPadrao.checked) {
             document.body.classList.remove(...temas);
             document.body.classList.add(temaPadrao.value);
             localStorage.setItem('tema', temaPadrao.value)
@@ -14,20 +14,20 @@ opcoesArvores.forEach(temaPadrao => {
 })
 
 //funçaõ para não adicionar o tema salvo do localStorage na pagina de login quando abrir a pagina.
-function aplicarTemaSalvo (){
-    if(!window.location.href.includes('/reflorestamento/index.html')){
-    let temaPadrao = localStorage.getItem('tema');
-    temaPadrao?document.body.classList.add(temaPadrao):null; //substitui um if
+function aplicarTemaSalvo() {
+    if (!window.location.href.includes('/reflorestamento/index.html')) {
+        let temaPadrao = localStorage.getItem('tema');
+        temaPadrao ? document.body.classList.add(temaPadrao) : null; //substitui um if
     }
 }
 aplicarTemaSalvo()
 
-if(btnLogin){
+if (btnLogin) {
     btnLogin.addEventListener('click', () => {
         let usuario = document.getElementById('usuario');
         let senha = document.getElementById('senha');
 
-        if(!usuario.value.trim() || !senha.value.trim()) {
+        if (!usuario.value.trim() || !senha.value.trim()) {
             alert('Preencha usuário e senha corretamente.');
             return;
         }
@@ -47,16 +47,16 @@ if(btnLogin){
 }
 
 let btnRegistro = document.querySelector('.btn-registro');
-if(btnRegistro){
-    btnRegistro.addEventListener('click', () =>{
+if (btnRegistro) {
+    btnRegistro.addEventListener('click', () => {
         let quantArvores = document.getElementById('quantidade-Arvores');
         let especieReflorestada = document.querySelector('input[name="arvores-reflorestar"]:checked');
 
-        if(!especieReflorestada){
+        if (!especieReflorestada) {
             alert('Selecione a espécie de arvore plantada.');
             return;
         }
-        if(!quantArvores.value.trim()){
+        if (!quantArvores.value.trim()) {
             alert('Preencha a quantidade de arvores plantadas.');
             return;
         }
@@ -80,11 +80,11 @@ let imgBroto = "imagens/broto.jpg";
 let imgCastanheiraJovem = "imagens/castanheira-jovem.jpg";
 let imgPerobaRosaJovem = "imagens/peroba-rosa-jovem.jpg";
 let imgPauBrasilJovem = "imagens/pau-brasil-jovem.jpg";
-let imgCastanheiraAdulta ="imagens/castanheira.jpg" ;
-let imgPerobaRosaAdulta ="imagens/peroba-rosa.jpg";
-let imgPauBrasilAdulto ="imagens/pau-brasil.jpg";
+let imgCastanheiraAdulta = "imagens/castanheira.jpg";
+let imgPerobaRosaAdulta = "imagens/peroba-rosa.jpg";
+let imgPauBrasilAdulto = "imagens/pau-brasil.jpg";
 
-if(window.location.href.includes('/reflorestamento/perfil.html')){
+if (window.location.href.includes('/reflorestamento/perfil.html')) {
     document.getElementById('nomeUsuarioPerfil').innerHTML = localStorage.getItem('usuario')
     let fotoPerfil = document.getElementById('foto-perfil');
     let quantidadeArvoresPlantadas = 300;
@@ -92,43 +92,43 @@ if(window.location.href.includes('/reflorestamento/perfil.html')){
     document.getElementById("quantidadeArvoresPlantadas").textContent = quantidadeArvoresPlantadas
     document.getElementById('area-texto').value = localStorage.getItem('bio')
 
-    if(quantidadeArvoresPlantadas <= 100){
+    if (quantidadeArvoresPlantadas <= 100) {
         fotoPerfil.src = imgSemente;
-    }else if(quantidadeArvoresPlantadas <= 300){
+    } else if (quantidadeArvoresPlantadas <= 300) {
         fotoPerfil.src = imgBroto
     }
 
-    if(localStorage.getItem('tema') == "tema-pau-brasil"){
-        if(quantidadeArvoresPlantadas > 300 &&  quantidadeArvoresPlantadas <= 700){
+    if (localStorage.getItem('tema') == "tema-pau-brasil") {
+        if (quantidadeArvoresPlantadas > 300 && quantidadeArvoresPlantadas <= 700) {
             fotoPerfil.src = imgPauBrasilJovem;
-        }else if(quantidadeArvoresPlantadas >700){
+        } else if (quantidadeArvoresPlantadas > 700) {
             fotoPerfil.src = imgPauBrasilAdulto;
         }
     }
-    if(localStorage.getItem('tema') == "tema-peroba-rosa"){
-        if(quantidadeArvoresPlantadas > 300 &&  quantidadeArvoresPlantadas <= 700){
+    if (localStorage.getItem('tema') == "tema-peroba-rosa") {
+        if (quantidadeArvoresPlantadas > 300 && quantidadeArvoresPlantadas <= 700) {
             fotoPerfil.src = imgPerobaRosaJovem;
-        }else if(quantidadeArvoresPlantadas >700){
+        } else if (quantidadeArvoresPlantadas > 700) {
             fotoPerfil.src = imgPerobaRosaAdulta;
         }
     }
-    if(localStorage.getItem('tema') == "tema-castanheira"){
-        if(quantidadeArvoresPlantadas > 300 &&  quantidadeArvoresPlantadas <= 700){
+    if (localStorage.getItem('tema') == "tema-castanheira") {
+        if (quantidadeArvoresPlantadas > 300 && quantidadeArvoresPlantadas <= 700) {
             fotoPerfil.src = imgCastanheiraJovem;
-        }else if(quantidadeArvoresPlantadas >700){
+        } else if (quantidadeArvoresPlantadas > 700) {
             fotoPerfil.src = imgCastanheiraAdulta;
         }
     }
     let btnSalvarBio = document.querySelector(".btn-salvar-bio");
-    if(btnSalvarBio){
-        btnSalvarBio.addEventListener('click', () =>{
+    if (btnSalvarBio) {
+        btnSalvarBio.addEventListener('click', () => {
             let areaTexto = document.getElementById("area-texto");
             localStorage.setItem('bio', areaTexto.value)
         })
     }
 }
 
-if(window.location.href.includes("/reflorestamento/destaques.html")){
+if (window.location.href.includes("/reflorestamento/destaques.html")) {
     //recupera localStorage em json e converte para JS
     const usuarios = JSON.parse(localStorage.getItem("usuariosDestaque"))
     //serve para organizar um item dentro dos usuarios
@@ -142,32 +142,75 @@ if(window.location.href.includes("/reflorestamento/destaques.html")){
         let nomeUsuario = document.getElementById(`usuario${index + 1}`);
         nomeUsuario.textContent = usuario.nome;
 
-        if(usuario.quantidadeArvoresPlantadas <= 100){
+        if (usuario.quantidadeArvoresPlantadas <= 100) {
             imgUsuario.src = imgSemente;
-        }else if(usuario.quantidadeArvoresPlantadas <= 300){
+        } else if (usuario.quantidadeArvoresPlantadas <= 300) {
             imgUsuario.src = imgBroto
         }
-        if (usuario.tema == "tema-pau-brasil"){
-            if(usuario.quantidadeArvoresPlantadas > 300 &&  usuario.quantidadeArvoresPlantadas <= 700){
+        if (usuario.tema == "tema-pau-brasil") {
+            if (usuario.quantidadeArvoresPlantadas > 300 && usuario.quantidadeArvoresPlantadas <= 700) {
                 imgUsuario.src = imgPauBrasilJovem;
-            }else if(usuario.quantidadeArvoresPlantadas >700){
+            } else if (usuario.quantidadeArvoresPlantadas > 700) {
                 imgUsuario.src = imgPauBrasilAdulto;
             }
         }
-        if(usuario.tema == "tema-peroba-rosa"){
-            if(usuario.quantidadeArvoresPlantadas > 300 &&  usuario.quantidadeArvoresPlantadas <= 700){
+        if (usuario.tema == "tema-peroba-rosa") {
+            if (usuario.quantidadeArvoresPlantadas > 300 && usuario.quantidadeArvoresPlantadas <= 700) {
                 imgUsuario.src = imgPerobaRosaJovem;
-            }else if(usuario.quantidadeArvoresPlantadas >700){
+            } else if (usuario.quantidadeArvoresPlantadas > 700) {
                 imgUsuario.src = imgPerobaRosaAdulta;
             }
         }
-        if(usuario.tema == "tema-castanheira"){
-            if(usuario.quantidadeArvoresPlantadas > 300 &&  usuario.quantidadeArvoresPlantadas <= 700){
+        if (usuario.tema == "tema-castanheira") {
+            if (usuario.quantidadeArvoresPlantadas > 300 && usuario.quantidadeArvoresPlantadas <= 700) {
                 imgUsuario.src = imgCastanheiraJovem;
-            }else if(usuario.quantidadeArvoresPlantadas >700){
+            } else if (usuario.quantidadeArvoresPlantadas > 700) {
                 imgUsuario.src = imgCastanheiraAdulta;
             }
         }
     })
 }
-//["tema-pau-brasil", "tema-peroba-rosa", "tema-castanheira"]
+
+if (window.location.href.includes("/reflorestamento/relatorio.html")) {
+    //recupera loscalStorage em json e converte para JS
+    const usuarios = JSON.parse(localStorage.getItem("usuariosDestaque"));
+
+    const tbody = document.getElementById('corpo-tabela')
+    tbody.innerHTML = '';
+
+
+    usuarios.forEach(usuario => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+        <td>${usuario.nome}</td>
+        <td>${usuario.tipoArvorePlantada}</td>
+        <td>${usuario.quantidadeArvoresPlantadas}</td>
+        `;
+
+        tbody.appendChild(linha)
+    })
+
+    const inputBucar = document.getElementById("input-busca-nome");
+    const tabelaUsuarios = document.getElementById("resultados-busca")
+
+    inputBucar.addEventListener('keyup', () => {
+        let expressao = inputBucar.value.toLowerCase();
+
+        let linhas = tbody.getElementsByTagName('tr');
+
+        for (let posicao in linhas) {
+            if (true === isNaN(posicao)) {
+                continue;
+            }
+
+            let conteudoDaLinha = linhas[posicao].innerHTML.toLowerCase();
+
+            if (true === conteudoDaLinha.includes(expressao)) {
+                linhas[posicao].style.display = '';
+            } else {
+                linhas[posicao].style.display = 'none'
+            }
+        }
+
+    })
+}
--- userclass.js
+++ userclass.js
@@ -1,18 +1,19 @@
 //serve para construir estrura de class
 class Usuario {
-    constructor(nome, tema, quantidadeArvoresPlantadas){
+    constructor(nome, tema, quantidadeArvoresPlantadas, tipoArvorePlantada){
         this.nome = nome;
         this.tema = tema;
         this.quantidadeArvoresPlantadas = quantidadeArvoresPlantadas;
+        this.tipoArvorePlantada = tipoArvorePlantada
     }
 }
 //adiciona objetos dentro da class
 let Usuarios = [
-    new Usuario("Carlos Motta", "tema-castanheira", 1000),
-    new Usuario("Maria Souza", "tema-pau-brasil", 580),
-    new Usuario("João Kozanda", "tema-peroba-rosa", 905),
-    new Usuario("Ana Prado", "tema-castanheira", 100),
-    new Usuario("Bruno Arruda", "tema-pau-brasil", 300)
+    new Usuario("Carlos Motta", "tema-castanheira", 1000, "Ipês"),
+    new Usuario("Maria Souza", "tema-pau-brasil", 580, "Angicos"),
+    new Usuario("João Kozanda", "tema-peroba-rosa", 905, "Aroeiras"),
+    new Usuario("Ana Prado", "tema-castanheira", 100, "Jequitibas"),
+    new Usuario("Bruno Arruda", "tema-pau-brasil", 300, "Peroba do campo")
 ]
 //salva objeto em JSON no localStorage
 localStorage.setItem("usuariosDestaque", JSON.stringify(Usuarios)); 
